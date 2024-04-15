1e5ce42583037e5575d78f7d963f2968c7f41ac6
Aristotel Paskalov <paskalov.aris@gmail.com>
2024-04-15 12:06:45 +0300

    feat: render mock sections on home screen

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,22 @@
 import { FC } from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { FlatList, ListRenderItemInfo, SafeAreaView, View } from 'react-native';
 import { styles } from './homeScreen.styles';
+import sections from '../../../sections.json';
+import { Section } from '../../models/Section';
+import { Carousel } from '../../components/Carousel/Carousel';
+
+const renderItem = ({ item }: ListRenderItemInfo<Section>) => {
+  return <Carousel {...item} key={item.id} />;
+};
 
 export const HomeScreen: FC = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Text style={styles.text}>Home screen</Text>
+      <View style={styles.wrapper}>
+        <FlatList
+          data={sections.sections as Section[]}
+          renderItem={renderItem}
+        />
       </View>
     </SafeAreaView>
   );
